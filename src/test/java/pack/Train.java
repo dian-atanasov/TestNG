31be567076a54ccc965ef21b913cc2604b34e0a0
Dian <Dian@DESKTOP-3A2N0I2>
2024-04-21 18:13:00 +0300

    Initial Commit

---
+++ src/test/java/pack/Train.java
@@ -0,0 +1,21 @@
+package pack;
+
+import org.testng.annotations.Test;
+
+public class Train {
+
+	@Test
+	public void Test01() {
+		System.out.println("Hello Test1");
+	}
+	
+	@Test
+	public void Test02() {
+		System.out.println("Hello Test2");
+	}
+	
+	@Test
+	public void Test03() {
+		System.out.println("Hello Test3");
+	}
+}
